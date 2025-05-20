5607c5332183f53cb9c6b5e3d02e101ff1336699
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-05-20 17:49:23 +0200

    Create Taskbaricons.ps1

---
+++ Taskbaricons.ps1
@@ -0,0 +1,58 @@
+$taskbar_layout =
+@"
+<?xml version="1.0" encoding="utf-8"?>
+<LayoutModificationTemplate
+    xmlns="http://schemas.microsoft.com/Start/2014/LayoutModification"
+    xmlns:defaultlayout="http://schemas.microsoft.com/Start/2014/FullDefaultLayout"
+    xmlns:start="http://schemas.microsoft.com/Start/2014/StartLayout"
+    xmlns:taskbar="http://schemas.microsoft.com/Start/2014/TaskbarLayout"
+    Version="1">
+  <CustomTaskbarLayoutCollection PinListPlacement="Replace">
+    <defaultlayout:TaskbarLayout>
+      <taskbar:TaskbarPinList>
+        <taskbar:DesktopApp DesktopApplicationLinkPath="C:\Program Files (x86)\AnyDesk\AnyDesk.exe" />
+        <taskbar:DesktopApp DesktopApplicationLinkPath="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" />
+        <taskbar:DesktopApp DesktopApplicationLinkPath="C:\Program Files\Google\Chrome\Application\chrome.exe" />
+        <taskbar:DesktopApp DesktopApplicationLinkPath="C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE" />
+        <taskbar:DesktopApp DesktopApplicationID="Microsoft.Windows.Explorer" />
+      </taskbar:TaskbarPinList>
+    </defaultlayout:TaskbarLayout>
+ </CustomTaskbarLayoutCollection>
+</LayoutModificationTemplate>
+"@
+
+# prepare provisioning folder
+[System.IO.FileInfo]$provisioning = "$($env:ProgramData)\provisioning\tasbar_layout.xml"
+if (!$provisioning.Directory.Exists) {
+    $provisioning.Directory.Create()
+}
+
+$taskbar_layout | Out-File $provisioning.FullName -Encoding utf8
+
+$settings = [PSCustomObject]@{
+    Path  = "SOFTWARE\Policies\Microsoft\Windows\Explorer"
+    Value = $provisioning.FullName
+    Name  = "StartLayoutFile"
+    Type  = [Microsoft.Win32.RegistryValueKind]::ExpandString
+},
+[PSCustomObject]@{
+    Path  = "SOFTWARE\Policies\Microsoft\Windows\Explorer"
+    Value = 1
+    Name  = "LockedStartLayout"
+} | Group-Object Path
+
+foreach ($setting in $settings) {
+    $registry = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey($setting.Name, $true)
+    if ($null -eq $registry) {
+        $registry = [Microsoft.Win32.Registry]::LocalMachine.CreateSubKey($setting.Name, $true)
+    }
+    $setting.Group | % {
+        if (!$_.Type) {
+            $registry.SetValue($_.name, $_.value)
+        }
+        else {
+            $registry.SetValue($_.name, $_.value, $_.type)
+        }
+    }
+    $registry.Dispose()
+}
